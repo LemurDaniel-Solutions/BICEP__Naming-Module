d0221fa344f00a901a262740c5afdbba7492f82e
LemurDaniel <49441790+LemurDaniel@users.noreply.github.com>
2025-03-14 17:50:01 +0100

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -3,40 +3,36 @@
 $resourceGroup = 'module.bicep-naming.examples'
 $location = 'Westeurope'
 
-New-AzResourceGroup -Name $resourceGroup -Location $location
+try {
+  New-AzResourceGroup -Name $resourceGroup -Location $location -Force -Confirm:$false
 
-
-
-Write-Host -ForegroundColor Yellow "Example 1"
-.\examples\example.naming.001\deploy.ps1
-
-Write-Host
-Write-Host
-Write-Host
-Read-Host -Prompt "Continue?"
-
-
-
-Write-Host -ForegroundColor Yellow "Example Errors"
-.\examples\example.naming.errors\deploy.ps1
-
-Write-Host
-Write-Host
-Write-Host
-Read-Host -Prompt "Continue?"
-
-
-
-Write-Host -ForegroundColor Yellow "Example Subscription Scope"
-.\examples\example.naming.subsScope\deploy.ps1
-
-Write-Host
-Write-Host
-Write-Host
-Read-Host -Prompt "Continue?"
-
-
-
-
-
-Remove-AzResourceGroup -Name $resourceGroup
+  Write-Host -ForegroundColor Yellow "Example 1"
+  .\examples\example.naming.001\deploy.ps1
+  
+  Write-Host
+  Write-Host
+  Write-Host
+  Read-Host -Prompt "Continue?"
+  
+  
+  
+  Write-Host -ForegroundColor Yellow "Example Errors"
+  .\examples\example.naming.errors\deploy.ps1
+  
+  Write-Host
+  Write-Host
+  Write-Host
+  Read-Host -Prompt "Continue?"
+  
+  
+  
+  Write-Host -ForegroundColor Yellow "Example Subscription Scope"
+  .\examples\example.naming.subsScope\deploy.ps1
+  
+  Write-Host
+  Write-Host
+  Write-Host
+  Read-Host -Prompt "Continue?"
+} finally {
+  Remove-AzResourceGroup -Name $resourceGroup -Force -Confirm:$false
+}
